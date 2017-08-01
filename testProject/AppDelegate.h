a209fa02f22ed666c8af865ace5ab2eb275b96e4
SAIGIT23 <30587074+SAIGIT23@users.noreply.github.com>
2017-08-01 09:39:49 +0800

    testiOS Project added

---
+++ testProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by Mobility on 01/08/2017.
+//  Copyright © 2017 Mobility. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
